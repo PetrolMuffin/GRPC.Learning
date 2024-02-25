1faf50ee9d83dcfd0b1c1aba8e5dd3ad7e2b0998
Petrol Muffin (Aleksandr Tereshchenko) <te.alex@outlook.com>
2024-02-25 23:08:46 +0100

    Create server

---
+++ Server/Server.Grpc/Services/MessageService.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Core;
+using Grpc.Messager;
+
+namespace Server.Grpc.Services;
+
+public sealed class MessageService : global::Grpc.Messager.MessageService.MessageServiceBase
+{
+    public override Task<Response> SendMessage(Message request, ServerCallContext context)
+    {
+        Console.WriteLine($"Received message from user {request.UserId}: {request.Message_}");
+        return Task.FromResult(new Response { Code = 200 });
+    }
+}
